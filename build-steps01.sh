b9d26f7aae1cbe6f5d92b3bdcaade3601f418387
Andre da Silva <andre.felix@cis.ntt.com>
2021-04-02 21:27:23 +0100

    Update build scripts

--- build-steps01.sh
+++ build-steps01.sh
@@ -2,8 +2,8 @@
 
 echo "delete clusters"
 
-minikube delete -p target-k8s
-minikube delete -p argocd-k8s
+#minikube delete -p target-k8s
+#minikube delete -p argocd-k8s
 
 echo "Create the clusters"
 
@@ -11,7 +11,7 @@ minikube start -p target-k8s
 minikube start -p argocd-k8s
 minikube addons -p argocd-k8s enable ingress
 
-echo "Set the argocd cluster as default"
+echo "Set the Argocd cluster as default"
 
 kubectl config use-context argocd-k8s
 
@@ -21,4 +21,4 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f install.yml
 
 echo "Listing pods"
-kubectl get pods -n argocd -w
+until (kubectl get pods -n argocd | grep -c Running) | grep -m 1 "5"; do : ; done
--- build-steps02.sh
+++ build-steps02.sh
@@ -2,11 +2,9 @@
 
 kubectl port-forward svc/argocd-server -n argocd 8080:443 &
 
-kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
-
 export ADM_PASSWORD=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)
 
-echo 'Adm Password: ' $ADM_PASSWORD
+echo 'Adm Password: '$ADM_PASSWORD
 
 echo "Using ArgoCD"
 
@@ -15,7 +13,6 @@ echo "Using ArgoCD"
 	argocd account update-password --current-password $ADM_PASSWORD  --new-password teco01
 
  	argocd cluster add target-k8s
-
 
 echo "Install demo Add App using ArgoCD"
 
@@ -42,18 +39,8 @@ kubectl port-forward svc/spring-petclinic -n default 9090:8080 &
 kubectl config use-context argocd-k8s
 
 kubectl apply -f ./ingress/argocd-server-ingress.yml
+kubectl apply -f ./ingress/argocd-server-ingress-grpc.yml
 
 echo "target-k8s -> "$(minikube -p target-k8s ip)
 echo "arogcd-k8s -> "$(minikube -p argocd-k8s ip)
 
-
-
-
-# http://localhost:8080
-# admin
-# argocd-server-5d7b59fcd-sk7g7
-
-# https://wiki-engr.mcp-services.net/display/CIDMS/Analysis+of+Automated+Deployment+Tooling
-# https://wiki-engr.mcp-services.net/display/CIDMS/2021-03-02+Nebula+Development+Meeting+-+agenda+and+notes
-# https://wiki-engr.mcp-services.net/display/CIDMS/2021-03-16+Nebula+Development+Meeting+-+agenda+and+notes
-# https://wiki-engr.mcp-services.net/display/CIDMS/Nebula+-+Argo+CD
